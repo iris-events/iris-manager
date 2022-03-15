91861cf36f3a931e82a12560fd061f0887a122d5
Nejc Tomažič <nejc.tomazic@global.id>
2022-03-15 10:05:38 +0100

    INFRA-1329: revert RoutingDetails back to class due to mocking

--- src/main/java/id/global/iris/manager/queue/operations/RoutingDetails.java
+++ src/main/java/id/global/iris/manager/queue/operations/RoutingDetails.java
@@ -1,4 +1,34 @@
 package id.global.iris.manager.queue.operations;
 
-public record RoutingDetails(String exchange, String routingKey, String countHeaderName) {
+public class RoutingDetails {
+    private final String exchange;
+    private final String routingKey;
+    private final String countHeaderName;
+
+    public RoutingDetails(String exchange, String routingKey, String countHeaderName) {
+        this.exchange = exchange;
+        this.routingKey = routingKey;
+        this.countHeaderName = countHeaderName;
+    }
+
+    public String exchange() {
+        return exchange;
+    }
+
+    public String routingKey() {
+        return routingKey;
+    }
+
+    public String countHeaderName() {
+        return countHeaderName;
+    }
+
+    @Override
+    public String toString() {
+        return "RoutingDetails[" +
+                "exchange=" + exchange + ", " +
+                "routingKey=" + routingKey + ", " +
+                "countHeaderName=" + countHeaderName + ']';
+    }
+
 }
