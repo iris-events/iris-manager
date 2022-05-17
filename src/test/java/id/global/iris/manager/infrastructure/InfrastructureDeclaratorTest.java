3d4dfc8fd6270295cac040055a9bb806815a67d4
Nejc Tomažič <nejc.tomazic@global.id>
2022-05-17 14:52:14 +0200

    INFRA-1506: declare snapshot exchange within infrastructure declaration

---
+++ src/test/java/id/global/iris/manager/infrastructure/InfrastructureDeclaratorTest.java
@@ -0,0 +1,80 @@
+package id.global.iris.manager.infrastructure;
+
+import static id.global.common.iris.constants.MessagingHeaders.QueueDeclaration.X_MESSAGE_TTL;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+
+import id.global.common.iris.constants.Exchanges;
+import id.global.common.iris.constants.Queues;
+import id.global.iris.manager.connection.ConnectionProvider;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+
+@QuarkusTest
+class InfrastructureDeclaratorTest {
+
+    @Inject
+    InfrastructureDeclarator infrastructureDeclarator;
+
+    @InjectMock
+    ConnectionProvider connectionProvider;
+
+    private Channel channel;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        channel = mock(Channel.class);
+        final var declareOk = mock(AMQP.Queue.DeclareOk.class);
+        when(channel.queueDeclare(anyString(), anyBoolean(), anyBoolean(), anyBoolean(), any())).thenReturn(declareOk);
+        when(connectionProvider.connect()).thenReturn(channel);
+    }
+
+    @Test
+    void declareBackboneInfrastructure() throws Exception {
+        infrastructureDeclarator.declareBackboneInfrastructure();
+
+        verify(channel).exchangeDeclare(Exchanges.ERROR.getValue(), BuiltinExchangeType.TOPIC, true);
+        verify(channel).exchangeDeclare(Exchanges.RETRY.getValue(), BuiltinExchangeType.DIRECT, true);
+        verify(channel).exchangeDeclare(Exchanges.DEAD_LETTER.getValue(), BuiltinExchangeType.TOPIC, true);
+        verify(channel).exchangeDeclare(Exchanges.FRONTEND.getValue(), BuiltinExchangeType.TOPIC, true);
+        verify(channel).exchangeDeclare(Exchanges.SESSION.getValue(), BuiltinExchangeType.TOPIC, true);
+        verify(channel).exchangeDeclare(Exchanges.USER.getValue(), BuiltinExchangeType.TOPIC, true);
+        verify(channel).exchangeDeclare(Exchanges.BROADCAST.getValue(), BuiltinExchangeType.TOPIC, true);
+        verify(channel).exchangeDeclare(Exchanges.SUBSCRIPTION.getValue(), BuiltinExchangeType.TOPIC, true);
+        verify(channel).exchangeDeclare(Exchanges.SNAPSHOT_REQUESTED.getValue(), BuiltinExchangeType.TOPIC, true);
+
+        verify(channel).queueDeclare(Queues.ERROR.getValue(), true, false, false, null);
+        verify(channel).queueDeclare(Queues.RETRY.getValue(), true, false, false, Map.of(X_MESSAGE_TTL, 5000));
+        verify(channel).queueDeclare(Queues.RETRY_WAIT_ENDED.getValue(), true, false, false, null);
+        verify(channel).queueDeclare(Queues.DEAD_LETTER.getValue(), true, false, false, null);
+        verify(channel).queueDeclare(Queues.SUBSCRIPTION.getValue(), true, false, false, null);
+
+        verify(channel).queueBind(Queues.ERROR.getValue(), Exchanges.ERROR.getValue(), "*");
+        verify(channel).queueBind(Queues.RETRY.getValue(), Exchanges.RETRY.getValue(), Queues.RETRY.getValue());
+        verify(channel).queueBind(Queues.RETRY_WAIT_ENDED.getValue(), Exchanges.RETRY.getValue(),
+                Queues.RETRY_WAIT_ENDED.getValue());
+        verify(channel).queueBind(Queues.DEAD_LETTER.getValue(), Exchanges.DEAD_LETTER.getValue(), "#");
+
+        verify(channel, times(6)).close();
+
+        verifyNoMoreInteractions(channel);
+    }
+
+}
